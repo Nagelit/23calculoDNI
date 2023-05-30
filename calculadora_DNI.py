d0b95b8680daa918a4ddf44b3beeef24904d8226
Nage <Nage>
2023-05-30 09:05:09 +0200

    calculo dni

--- calculadora_DNI.py
+++ calculadora_DNI.py
@@ -1,3 +1,9 @@
+n = int(input ("introduzca el numero del DNI:"))
+letra = "T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"
+print("la letra del DNI es:", letra[n%23])
+
 def calculadora_dni(numero_dni):
-    letra = "A"
-    return letra
+     letra = "A"
+     return letra
+
+
--- main.py
+++ main.py
@@ -7,18 +7,18 @@ n = int(input ("introduzca el numero del DNI:"))
 letra = "T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"
 print("la letra del DNI es:", letra[n%23])
 
-#from calculadora_DNI import calculadora_dni
-#def pide_un_numero():
-#   msg = "Dame un num: "
-#   entrada = ''
-#   invalido = False
-#   numero = -1
-#   while not entrada.isnumeric():
-#       if invalido:
-#           print("Obligatoriamente debe ser un número")
-#       invalido = True
-#       entrada = input(msg)
-#   return int(entrada)
-#numero_DNI = pide_un_numero Max8Digitos
-#letra_dni = calculadora_DNI(numero_dni)
-#print(f"DNI calculado: {numero_DNI}-{letra_dni}")
+
+def pide_un_numeroMax8Digitos():
+  msg = "Dame un num: "
+  entrada = ''
+  invalido = False
+  numero = -1
+  while not entrada.isnumeric():
+      if invalido:
+          print("Obligatoriamente debe ser un número")
+      invalido = True
+      entrada = input(msg)
+  return int(entrada)
+numero_DNI = pide_un_numeroMax8Digitos
+letra_dni = calculadora_dni(numero_dni)
+print(f"DNI calculado: {numero_DNI}-{letra_dni}")
